85badcdfeaab5f7eac4e6e5b237b66b0e901db36
lukethorvilson <luke.thorvilson@wsu.edu>
2024-08-22 12:49:17 -0700

    Switched to env vars.

--- chatBotAI-app/src/api/OpenAI.js
+++ chatBotAI-app/src/api/OpenAI.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
 export const openai = new OpenAI({
-    organization: "org-JKIEYK1ZUXPF7sCeHAfxrIOG",
-    project: "proj_kh5iVltQ8PERE6wqncoYpCrf",
+  organization: import.meta.env.ORG_ID,
+  project: import.meta.env.PROJECT_ID,
 });
