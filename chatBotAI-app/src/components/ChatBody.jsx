dbef3a74c41e4019fd633bbd510ca60656311b80
lukethorvilson <luke.thorvilson@wsu.edu>
2024-08-24 15:09:30 -0700

    Fixed layout chat issues.

--- chatBotAI-app/src/components/ChatBody.jsx
+++ chatBotAI-app/src/components/ChatBody.jsx
@@ -1,12 +1,28 @@
 function ChatBody({ chat, response, isGenerating, error }) {
-  console.log(response, isGenerating, error)
+  console.log(response, isGenerating, error);
   return (
     <>
-      <div className="grid h-fit w-full grid-cols-1">
-        {chat.map((chatItem, i) => <div key={i}>{chatItem.role === "user" ? `You: ${chatItem.content}` : `🤖: ${chatItem.content}`}</div>)}
-        {error && <div><span className="text-red-500">calculations failed</span></div>}
-        {isGenerating && <div>...calculating bzzt-beep-boop🤖💭</div>}
-        {response && !isGenerating && <div>🤖: {response.content}</div>}
+      <div className="mx-auto h-[80%] w-full overflow-auto rounded border-4 border-green-400">
+        {chat.map((chatItem, i) => (
+          <div className="block h-fit w-full" key={i}>
+            {chatItem.role === "user"
+              ? `You: ${chatItem.content}`
+              : `🤖: ${chatItem.content}`}
+          </div>
+        ))}
+        {error && (
+          <div className="block h-fit w-full">
+            <span className="text-red-500">calculations failed</span>
+          </div>
+        )}
+        {isGenerating && (
+          <div className="block h-fit w-full">
+            ...calculating bzzt-beep-boop🤖💭
+          </div>
+        )}
+        {response && !isGenerating && (
+          <div className="block h-fit w-full">🤖: {response.content}</div>
+        )}
       </div>
     </>
   );
--- chatBotAI-app/src/components/Input.jsx
+++ chatBotAI-app/src/components/Input.jsx
@@ -5,23 +5,29 @@ const placeholders = [
   "Hi Chatbot!",
 ];
 function randomPlaceholder() {
-    const random = Math.floor(Math.random() * 4);
-    return placeholders[random];
-  }
+  const random = Math.floor(Math.random() * 4);
+  return placeholders[random];
+}
 function Input({ userInput, setUserInput, handleSubmit }) {
   return (
     <>
-        <form className="absolute bottom-0 flex h-16 w-full gap-4 bg-gray-200 pb-2 pl-6 lg:justify-center" onSubmit={(e) => handleSubmit(e)}>
-          <input
-            className="h-[80%] w-[75%] overflow-auto rounded-3xl border-2 border-green-700 bg-gray-200 px-4 text-[12px] focus:border-2 focus:border-green-700 focus:ring-4 focus:ring-green-500 sm:text-[14px] md:w-[85%] lg:w-[70%] lg:text-[16px]"
-            placeholder={randomPlaceholder()}
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-          ></input>
-          <button type="submit" className="h-[70%] w-[15%] rounded-3xl bg-green-500 text-white transition-colors hover:bg-green-600 md:w-[10%] lg:w-[100px]">
-            Send
-          </button>
-        </form>
+      <form
+        className="absolute bottom-0 flex h-16 w-[100%] gap-2 bg-gray-200 pb-2 lg:justify-center"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <input
+          className="h-[80%] w-[75%] overflow-auto rounded-3xl border-2 border-green-700 bg-gray-200 px-4 text-[12px] focus:border-2 focus:border-green-700 focus:ring-4 focus:ring-green-500 sm:text-[14px] md:w-[85%] lg:w-[70%] lg:text-[16px]"
+          placeholder={randomPlaceholder()}
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        ></input>
+        <button
+          type="submit"
+          className="h-[70%] w-[15%] rounded-3xl bg-green-500 text-white transition-colors hover:bg-green-600 md:w-[10%] lg:w-[100px]"
+        >
+          Send
+        </button>
+      </form>
     </>
   );
 }
