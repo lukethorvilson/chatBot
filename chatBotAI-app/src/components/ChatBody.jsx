ad147366e1492fef8884a8c23a467468337f839a
lukethorvilson <luke.thorvilson@wsu.edu>
2024-08-26 15:01:14 -0700

    Finished the basic features of chatbot.

--- chatBotAI-app/src/components/ChatBody.jsx
+++ chatBotAI-app/src/components/ChatBody.jsx
@@ -2,14 +2,30 @@ function ChatBody({ chat, response, isGenerating, error }) {
   console.log(response, isGenerating, error);
   return (
     <>
-      <div className="h-[80%] mx-auto w-[90%] overflow-auto rounded border-4 border-green-400">
-        {chat.map((chatItem, i) => (
-          <div className="block h-fit w-full" key={i}>
-            {chatItem.role === "user"
-              ? `You: ${chatItem.content}`
-              : `🤖: ${chatItem.content}`}
+      <div className="mx-auto h-[80%] w-[90%] overflow-auto rounded border-4 border-green-400">
+        {!chat.length && (
+          <div className="mt-8 block text-center text-lg opacity-35 md:text-xl">
+            Begin chatting with ChatBot!
           </div>
-        ))}
+        )}
+        {chat.map((chatItem, i) => {
+          if (chatItem.role === "user") {
+            return (
+              <div
+                className="block px-2 h-fit w-full"
+                key={i}
+              >{`You: ${chatItem.content}`}</div>
+            );
+          }
+          if (chatItem.role === "assistant") {
+            return (
+              <div
+                className="block px-2 h-fit w-full"
+                key={i}
+              >{`🤖: ${chatItem.content}`}</div>
+            );
+          }
+        })}
         {error && (
           <div className="block h-fit w-full">
             <span className="text-red-500">calculations failed</span>
@@ -21,7 +37,7 @@ function ChatBody({ chat, response, isGenerating, error }) {
           </div>
         )}
         {response && !isGenerating && (
-          <div className="block h-fit w-full">🤖: {response.content}</div>
+          <div className="block px-2 h-fit w-full">🤖: {response.content}</div>
         )}
       </div>
     </>
--- chatBotAI-app/src/hooks/useChatAI.js
+++ chatBotAI-app/src/hooks/useChatAI.js
@@ -2,49 +2,53 @@
 import { useEffect, useState } from "react";
 import { openai } from "../api/OpenAI";
 
-function useChatAI(chat, setChat) {
+const tools = [
+  {
+    type: "function",
+    function: {
+      name: "get_date",
+      description:
+        "Get the current date for the user. Call this whenever you need to know the current date of today, for example when the user asks, 'What is the date today?'",
+      parameters: {},
+    },
+  },
+];
+
+const getDate = function () {
+  return new Date();
+};
+
+function useChatAI(chat) {
   const [isGenerating, setIsGenerating] = useState(false);
   const [error, setError] = useState(null);
   const [response, setResponse] = useState("");
-  const [isTimedOut, setIsTimedOut] = useState(false);
+  console.log(chat)
   useEffect(
     function () {
-      let timeoutId;
       async function generateCompletion() {
-        // so not too many requests are made in development
-        if (!isTimedOut && chat.length) {
-          try {
-            setIsGenerating(true);
-            setIsTimedOut(true);
-            console.log("Chat timeout started");
-            timeoutId = setTimeout(() => {
-              setIsTimedOut(false);
-              console.log("Chat timeout ended.");
-            }, 10000);
-            const completion = await openai.chat.completions.create({
-              messages: chat,
-              model: "gpt-3.5-turbo",
-            });
-            setResponse(completion.choices[0].message);
-            setIsGenerating(false);
-          } catch (err) {
-            setIsGenerating(false);
-            setError(`Chatbot Error: ${err.message}`);
-          }
+        try {
+          setIsGenerating(true);
+          const completion = await openai.chat.completions.create({
+            messages: chat,
+            model: "gpt-3.5-turbo",
+            tools: tools,
+          });
+          // set response to display to the user
+          setResponse(completion.choices[0].message);
+          setIsGenerating(false);
+        } catch (err) {
+          setIsGenerating(false);
+          setError(`Chatbot Error: ${err.message}`);
         }
       }
+      // handle if the effect should be ran again based on if the chat was a tool response or user and is not empty chat
       if (chat.length > 0 && chat[chat.length - 1]?.role === "user") {
         generateCompletion();
-      }
-      return () => {
-        if(timeoutId) {
-          clearTimeout(timeoutId);
-        }
       }
     },
     [chat],
   );
-  return [response,setResponse, isGenerating, error];
+  return [response, setResponse, isGenerating, error];
 }
 
 export default useChatAI;
