4a1effcc768a1bc27857e2e974c59e087805d39b
lukethorvilson <luke.thorvilson@wsu.edu>
2024-09-06 12:30:17 -0700

    Finished v1 of chatbot app.

--- chatBotAI-app/src/components/ChatBody.jsx
+++ chatBotAI-app/src/components/ChatBody.jsx
@@ -2,7 +2,7 @@ function ChatBody({ chat, response, isGenerating, error }) {
   console.log(response, isGenerating, error);
   return (
     <>
-      <div className="mx-auto h-[80%] w-[90%] overflow-auto rounded border-4 border-green-400">
+      <div className="font-body mx-auto h-[80%] w-[90%] overflow-auto rounded border-4 border-green-400 p-2">
         {!chat.length && (
           <div className="mt-8 block text-center text-lg opacity-35 md:text-xl">
             Begin chatting with ChatBot!
@@ -12,17 +12,20 @@ function ChatBody({ chat, response, isGenerating, error }) {
           if (chatItem.role === "user") {
             return (
               <div
-                className="block px-2 h-fit w-full"
+                className="block h-fit w-full px-2 text-2xl font-semibold sm:text-xl mb-1"
                 key={i}
-              >{`You: ${chatItem.content}`}</div>
+              >
+                <span className="mr-3 font-bold">You:</span>
+                {`${chatItem.content}`}
+              </div>
             );
           }
           if (chatItem.role === "assistant") {
             return (
               <div
-                className="block px-2 h-fit w-full"
+                className="block h-fit w-full px-2 text-2xl sm:text-xl font-semibold mb-1"
                 key={i}
-              >{`🤖: ${chatItem.content}`}</div>
+              >🤖: {chatItem.content}</div>
             );
           }
         })}
@@ -32,12 +35,12 @@ function ChatBody({ chat, response, isGenerating, error }) {
           </div>
         )}
         {isGenerating && (
-          <div className="block h-fit w-full">
-            ...calculating bzzt-beep-boop🤖💭
+          <div className="block h-fit w-full font-medium text-xl sm:text-2xl mb-1">
+            ...calculating--bzzt-beep-boop 🤖💭
           </div>
         )}
         {response && !isGenerating && (
-          <div className="block px-2 h-fit w-full">🤖: {response.content}</div>
+          <div className="block h-fit w-full px-2 text-2xl sm:text-xl font-semibold mb-1"><span className="mr-2">🤖:</span> {response.content}</div>
         )}
       </div>
     </>
--- chatBotAI-app/src/hooks/useChatAI.js
+++ chatBotAI-app/src/hooks/useChatAI.js
@@ -2,27 +2,12 @@
 import { useEffect, useState } from "react";
 import { openai } from "../api/OpenAI";
 
-const tools = [
-  {
-    type: "function",
-    function: {
-      name: "get_date",
-      description:
-        "Get the current date for the user. Call this whenever you need to know the current date of today, for example when the user asks, 'What is the date today?'",
-      parameters: {},
-    },
-  },
-];
-
-const getDate = function () {
-  return new Date();
-};
-
 function useChatAI(chat) {
   const [isGenerating, setIsGenerating] = useState(false);
   const [error, setError] = useState(null);
   const [response, setResponse] = useState("");
   console.log(chat)
+  console.log(response)
   useEffect(
     function () {
       async function generateCompletion() {
@@ -31,7 +16,6 @@ function useChatAI(chat) {
           const completion = await openai.chat.completions.create({
             messages: chat,
             model: "gpt-3.5-turbo",
-            tools: tools,
           });
           // set response to display to the user
           setResponse(completion.choices[0].message);
